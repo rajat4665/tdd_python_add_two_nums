83397206e004a603673a07b2a4a137c2937d12b4
rajat4665 <rajat4665@gmail.com>
2025-02-04 00:08:50 +0530

    handeled dynamic delimeter, handled nagetive number case , igronred number if its gt 1000

--- add_sum.py
+++ add_sum.py
@@ -7,16 +7,34 @@ def add(numbers: str) -> int:
     # edge case handled when empty string came as input
     if not numbers:
         return 0
+
+    # default delimiter , and \n
+    delimiter = ',|\n'
+
+    # custom delimiter case
+    if numbers.startswith("//"):
+        parts = numbers.split("\n", 1)
+        delimiter = re.escape(parts[0][2:])  # Extract delimiter after '//'
+        numbers = parts[1]
+
+    list_of_nums = re.split(delimiter, numbers)
     
-    # Split using both ',' and '\n' as delimiters
-    list_of_nums = re.split(r'[,\n]', numbers)
     result = 0
     for num in list_of_nums:
-        result += int(num)
-    
+        num = int(num)
+        if num > 0 and num < 1001:
+            result += int(num)
+        elif num < 0:
+            raise ValueError(f'Negative numbers are not allowed: {num}')
     return result
 
 
 if __name__ == '__main__':
     print(add('1,2,3,4,5'))  # output: 15
     print(add("1\n2,3"))  # output: 6
+    print(add("//;\n1;2;3;9"))
+    try:
+        print(add("1,-2,3"))
+    except ValueError as e:
+        print(e)
+    print(add("1001,2,3"))  # output: 5
